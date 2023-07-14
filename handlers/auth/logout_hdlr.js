9d2bfbf128f1f8ef1d14e7a4464b906254c284b7
Chandler Chilvers <cwchilvers@gmail.com>
2023-07-13 21:30:47 -0700

    Fixed minor issue in login handler

--- handlers/auth/logout_hdlr.js
+++ handlers/auth/logout_hdlr.js
@@ -11,5 +11,6 @@ const logout = async () => {
     }
 };
 
-document.querySelector('#logout')
-.addEventListener('click', logout);
+document
+    .querySelector('#logout')
+    .addEventListener('click', logout);
