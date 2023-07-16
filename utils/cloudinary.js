8853ef854f0905b50735c1a83936e1802e22d266
Jorge Duarte <sadujorge@gmail.com>
2023-07-16 15:00:47 -0700

    Added ability to capture cloudinary upload  url so that it can be stored in db, retrieved, and rendered

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -5,13 +5,21 @@ let cloudinaryWidget = cloudinary.createUploadWidget(
     cloudName: process.env.CLOUD_NAME, 
     uploadPreset: process.env.UPLOAD_PRESET, 
     folder: process.env.CLOUD_FOLDER, 
-    // cropping: true
     }, 
     (error, result) => { 
-        console.log(error, result) 
+        // console.log(error, result);
+        if (!error && result && result.event === "success") {
+            const url = result.info.secure_url; // Extract the URL from the response.
+            document.getElementById('url').value = url; // Set the URL as the value of the 'url' field in the form. We will need to hide or grayout this part of the form.
+            const uploadButton = document.getElementById('upload_widget');
+            uploadButton.style.backgroundColor = 'lightgreen';
+            uploadButton.style.pointerEvents = 'none';
+            uploadButton.innerHTML = 'Upload Successful!';
+        }
     }
 );
 
 document.getElementById('upload_widget').addEventListener('click', function() {
     cloudinaryWidget.open();
 }, false);
+
