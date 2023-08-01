392bfc5f47f876799c5c0482a3f6f8d7c1988a87
Chandler Chilvers <cwchilvers@gmail.com>
2023-08-01 15:07:13 -0700

    Added error messages for incorrect login & password length on sign up

--- handlers/forms/signup_hdlr.js
+++ handlers/forms/signup_hdlr.js
@@ -3,12 +3,20 @@ const signup = async (event) => {
 
     const username = document.querySelector('#username-input').value.trim();
     const password = document.querySelector('#password-input').value.trim();
+    const errorMessage = document.querySelector('#error-message');
 
     if (username && password) {
+        // Invalid password length
+        if (password.length < 8) {
+            errorMessage.textContent = 'Password must be at least 8 characters long.';
+            errorMessage.style.display = 'block';
+            return;
+        }
+
         const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
